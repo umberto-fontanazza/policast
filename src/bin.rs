6d0d490e0b96968c8f9beceec49592015436a118
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-07-28 19:55:21 +0200

    Remove ffmpeg-next, it fails to build, add os specific hello

--- src/bin.rs
+++ src/bin.rs
@@ -2,6 +2,8 @@ use mpsc::{caster, player};
 use std::env;
 
 pub fn main() {
+    os_hello();
+
     let args: Vec<String> = env::args().collect();
     let terminal = &args[1];
     println!("{:?}", terminal);
@@ -11,5 +13,17 @@ pub fn main() {
         player::setup();
     } else {
         println!("Unrecognised command line arg");
+    }
+}
+
+fn os_hello() {
+    if cfg!(target_os = "macos") {
+        println!("Think different!");
+    } else if cfg!(target_os = "windows") {
+        println!("We like blue screens");
+    } else if cfg!(target_os = "linux") {
+        println!("We don't like the government!");
+    } else {
+        println!("We are not supported!");
     }
 }
