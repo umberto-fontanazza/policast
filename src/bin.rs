c9ab52e92edf2efdbc6fbd0f4be5df0211b3ad99
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-07-28 21:19:53 +0200

    Moving main.rs to bin.rs

--- src/bin.rs
+++ src/bin.rs
@@ -1,29 +1,31 @@
-use mpsc::{caster, player};
+use mpsc::{capture, caster, player};
 use std::env;
+use std::thread;
 
 pub fn main() {
-    os_hello();
-
     let args: Vec<String> = env::args().collect();
-    let terminal = &args[1];
-    println!("{:?}", terminal);
-    if terminal == "caster" {
-        caster::setup();
-    } else if terminal == "player" {
+    let peer_role = &args[1];
+    if peer_role == "caster" {
+        // caster::setup();
+        setup_stream();
+    } else if peer_role == "player" {
         player::setup();
     } else {
         println!("Unrecognised command line arg");
     }
 }
 
-fn os_hello() {
-    if cfg!(target_os = "macos") {
-        println!("Think different!");
-    } else if cfg!(target_os = "windows") {
-        println!("We like blue screens");
-    } else if cfg!(target_os = "linux") {
-        println!("We don't like the government!");
-    } else {
-        println!("We are not supported!");
-    }
+fn setup_stream() {
+    let video_width = 800;
+    let video_height = 600;
+    let x = 200;
+    let y = 200;
+
+    let (capture_process, stop_sender) =
+        capture::start_screen_capture(video_width, video_height, x, y);
+    println!("Screen capture started successfully.");
+
+    //TODO: STOP NON FUNZIONA
+    thread::sleep(std::time::Duration::from_secs(2));
+    capture::stop_screen_capture(capture_process, stop_sender);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod capture;
 pub mod caster;
 pub mod player;
 
