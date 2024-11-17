1995dad687ab0ae6b8d16a69b6e1697305283f1e
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-17 18:01:03 +0100

    Recycle texture when rendering buffer into image

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,8 @@
 use crate::hotkey::HotkeyManager;
-use eframe;
+use eframe::{self};
+use egui::{Color32, TextureHandle};
+
+const VIDEO_SIZE: [usize; 2] = [1920, 1080];
 
 #[derive(Default)]
 enum Route {
@@ -9,14 +12,44 @@ enum Route {
     PlayerRoot,
 }
 
-#[derive(Default)]
 pub struct Gui {
     route: Route,
+    video_texture: TextureHandle,
+}
+
+impl Gui {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // egui_extras::install_image_loaders(ctx);
+        Self {
+            route: Route::default(),
+            video_texture: cc.egui_ctx.load_texture(
+                "video-tex",
+                egui::ColorImage {
+                    size: VIDEO_SIZE,
+                    pixels: vec![Color32::BLACK; VIDEO_SIZE[0] * VIDEO_SIZE[1]],
+                },
+                egui::TextureOptions::NEAREST,
+            ),
+        }
+    }
+
+    fn render_video_frame(&mut self, ctx: &egui::Context, ui: &mut egui::Ui, pixels: Vec<Color32>) {
+        self.video_texture.set(
+            egui::ColorImage {
+                size: VIDEO_SIZE,
+                pixels,
+            },
+            egui::TextureOptions::NEAREST,
+        );
+        let size = self.video_texture.size_vec2();
+        let sized_texture = egui::load::SizedTexture::new(&self.video_texture, size);
+        ui.add(egui::Image::new(sized_texture).fit_to_exact_size(size));
+    }
 }
 
 impl eframe::App for Gui {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
             match self.route {
                 Route::SelectRole => {
                     ui.heading("Select your role");
@@ -37,6 +70,8 @@ impl eframe::App for Gui {
                 }
                 Route::PlayerRoot => {
                     ui.heading("Player root");
+                    let pixels = vec![Color32::BLACK; VIDEO_SIZE[0] * VIDEO_SIZE[1]];
+                    self.render_video_frame(ctx, ui, pixels);
                 }
             }
         });
