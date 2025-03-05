22b759ffedcff7e86de4d7c2ec1194a330e64130
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-03-05 13:50:56 +0100

    Add selected feedback for device

--- src/gui/caster_controls/device_selector.rs
+++ src/gui/caster_controls/device_selector.rs
@@ -3,12 +3,18 @@ use egui::{Align, Layout};
 
 impl Gui {
     pub fn device_selector(&mut self, ui: &mut egui::Ui) {
+        let selected_device = self.video_caster.get_selected_device();
         ui.with_layout(Layout::left_to_right(Align::LEFT), |ui| {
             self.video_caster
                 .get_capture_devices()
                 .into_iter()
                 .for_each(|(index, name)| {
-                    if ui.button(name).clicked() {
+                    let button = egui::Button::new(&name).selected(
+                        selected_device
+                            .as_ref()
+                            .is_some_and(|device| device.eq(&index)),
+                    );
+                    if ui.add(button).clicked() {
                         self.video_caster
                             .set_selected_device(index)
                             .expect("Couldn't set the selected device");
