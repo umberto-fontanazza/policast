fb7ab0c7974cd24843210ad287ebf1250d342469
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-04-03 13:03:37 +0200

    Previous commit

---
+++ src/gui/caster_controls/preview.rs
@@ -0,0 +1,25 @@
+use crate::gui::Route;
+
+use super::Gui;
+use egui::{ColorImage, Context};
+
+impl Gui {
+    pub fn recording_controls(&mut self, ui: &mut egui::Ui, ctx: &Context) {
+        if self.first_route_render {
+            self.preview_texture =
+                Some(ctx.load_texture("preview", ColorImage::default(), Default::default()));
+            if !self.capturer.is_recording() {
+                self.capturer.start_recording();
+            }
+        }
+        if ui.button("Back to device selection").clicked() {
+            self.capturer.stop_recording();
+            self.capturer
+                .set_selected_device(None)
+                .expect("Couldn't clear device selection");
+            self.route_to(Route::CasterDeviceSelection);
+        }
+        self.capturer
+            .render(ui, ctx, self.preview_texture.as_mut().unwrap());
+    }
+}
