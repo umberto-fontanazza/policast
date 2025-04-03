26d2b8bd6a557371dfa86629a98b1b9d3ef1339d
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-04-03 13:18:29 +0200

    Minor refactor

--- src/gui/caster_controls/preview.rs
+++ src/gui/caster_controls/preview.rs
@@ -20,25 +20,22 @@ impl Gui {
             self.route_to(Route::CasterDeviceSelection);
         }
 
-        match self.capturer.is_recording {
-            true => {
-                let texture = self
-                    .preview_texture
-                    .as_mut()
-                    .expect("Texture should be set");
-                let frame_receiver = self.capturer.frame_receiver();
-                let frame = frame_receiver.recv().unwrap();
-                util::update_texture(texture, frame);
-                ui.add(
-                    Image::new(&(*texture))
-                        .maintain_aspect_ratio(true)
-                        .fit_to_fraction(Vec2::new(1.0, 1.0)),
-                );
-                ctx.request_repaint();
-            }
-            false => {
-                ui.label("Capturer not recoding");
-            }
+        if self.capturer.is_recording {
+            let texture = self
+                .preview_texture
+                .as_mut()
+                .expect("Texture should be set");
+            let frame_receiver = self.capturer.frame_receiver();
+            let frame = frame_receiver.recv().unwrap();
+            util::update_texture(texture, frame);
+            ui.add(
+                Image::new(&(*texture))
+                    .maintain_aspect_ratio(true)
+                    .fit_to_fraction(Vec2::new(1.0, 1.0)),
+            );
+            ctx.request_repaint();
+        } else {
+            ui.label("Capturer not recoding");
         }
     }
 }
