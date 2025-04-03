bba34c7d98fb4ac53a63ed0247b005ed0b9a4131
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-04-03 13:00:46 +0200

    Start recording immediately

--- src/gui/caster_controls/recording_controls.rs
+++ src/gui/caster_controls/recording_controls.rs
@@ -7,23 +7,15 @@ impl Gui {
     pub fn recording_controls(&mut self, ui: &mut egui::Ui, ctx: &Context) {
         if self.first_route_render {
             self.preview_texture =
-                Some(ctx.load_texture("preview", ColorImage::default(), Default::default()))
+                Some(ctx.load_texture("preview", ColorImage::default(), Default::default()));
+            self.capturer.start_recording();
         }
         if ui.button("Back to device selection").clicked() {
+            self.capturer.stop_recording();
             self.capturer
                 .set_selected_device(None)
                 .expect("Couldn't clear device selection");
             self.route_to(Route::CasterDeviceSelection);
-        }
-        if !self.capturer.is_recording() {
-            if ui.button("Start Recording").clicked() {
-                //TODO: error management
-                self.capturer.start_recording();
-            }
-        } else {
-            if ui.button("Stop Recording").clicked() {
-                self.capturer.stop_recording();
-            }
         }
         self.capturer
             .render(ui, ctx, self.preview_texture.as_mut().unwrap());
