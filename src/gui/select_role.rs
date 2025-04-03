f8fe6b6e76a9bdfa5ffbf033c4cd8de0e2b7fa16
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-04-03 19:37:43 +0200

    Fix

--- src/gui/select_role.rs
+++ src/gui/select_role.rs
@@ -3,9 +3,8 @@ use super::{Gui, Route};
 impl Gui {
     pub fn select_role(&mut self, ui: &mut egui::Ui) {
         if ui.button("Caster").clicked() {
-            return self.route_to(Route::CasterDeviceSelection);
-            // Carica i dispositivi di cattura all'ingresso
             self.capturer.set_capture_devices();
+            return self.route_to(Route::CasterDeviceSelection);
         }
         if ui.button("Player").clicked() {
             return self.route_to(Route::PlayerControls);
