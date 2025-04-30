08e5f601372409bbf629a74ac0612f4fdeda8ae7
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-04-30 20:41:54 +0200

    Minor refactor

--- src/gui/player_controls.rs
+++ src/gui/player_controls.rs
@@ -14,38 +14,45 @@ impl Gui {
         ui.label("Enter the M3U link to play the video:");
 
         // Text field to input the M3U link
-        ui.add(TextEdit::singleline(&mut self.video_link).hint_text("Enter M3U link"));
+        ui.add(TextEdit::singleline(&mut self.video_link).hint_text("Enter M3U playlist link"));
 
-        if ui
-            .button(if self.playback.status() == PlaybackStatus::Paused {
-                "Resume"
-            } else {
-                "Play"
-            })
-            .clicked()
-        {
-            if !self.video_link.is_empty() {
-                self.playback.set_video_link(self.video_link.clone());
-                let save_path = self
-                    .settings
-                    .borrow()
-                    .player_save_dir
-                    .as_ref()
-                    .map(|path| path.clone());
-                self.playback.play(save_path);
-            } else {
-                ui.label("Enter a valid link!");
-            }
-        }
-
-        if ui.button("Stop").clicked() {
-            self.playback.stop();
-        }
-
-        if ui.button("Pause").clicked() {
-            self.playback.pause();
-        }
+        ui.horizontal(|ui| {
+            match self.playback.status() {
+                PlaybackStatus::Stopped => {
+                    if ui.button("Play").clicked() {
+                        self._playback_play();
+                    };
+                }
+                PlaybackStatus::Playing => {
+                    if ui.button("Stop").clicked() {
+                        self.playback.stop();
+                    }
+                    if ui.button("Pause").clicked() {
+                        self.playback.pause();
+                    }
+                }
+                PlaybackStatus::Paused => {
+                    if ui.button("Stop").clicked() {
+                        self.playback.stop();
+                    }
+                    if ui.button("Resume").clicked() {
+                        self.playback.play(None);
+                    }
+                }
+            };
+        });
 
         self.playback.render(ui, ctx);
+    }
+
+    fn _playback_play(&mut self) {
+        self.playback.set_video_link(self.video_link.clone());
+        let save_path = self
+            .settings
+            .borrow()
+            .player_save_dir
+            .as_ref()
+            .map(|path| path.clone());
+        self.playback.play(save_path);
     }
 }
